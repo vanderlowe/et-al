e09be3477831a147529c574f99cf31e17811f7ed
Ilmo van der Lowe <github@vanderlowe.com>
2013-01-29 07:49:47 +0000

    First commit

---
+++ Mendeley API/ROAuth.R
@@ -0,0 +1,22 @@
+require(ROAuth)
+
+reqURL <- "http://api.mendeley.com/oauth/request_token/"
+accessURL <- "http://api.mendeley.com/oauth/access_token/"
+authURL <- "http://api.mendeley.com/oauth/authorize/"
+
+cKey <- Sys.getenv("MENDELEY_KEY") # Add the actual key to .Rprofile.site file as a environment variable
+cSecret <- Sys.getenv("MENDELEY_SECRET") # Add the actual secret to .Rprofile.site file as a environment variable
+
+testURL <- "http://api.mendeley.com/oapi/documents/search/author:keltner/"
+
+credentials <- OAuthFactory$new(consumerKey=cKey,
+                                consumerSecret=cSecret,
+                                accessURL=accessURL,
+                                authURL=authURL,
+                                requestURL=reqURL,
+                                needsVerifier = FALSE)
+credentials$handshake()
+
+## the GET isn’t strictly necessary as that’s the default
+credentials$OAuthRequest(testURL, "GET")
+## End(Not run)
